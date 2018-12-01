7b436ca10125c7056749dbc7ae48f89714eff1cb
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2018-12-01 20:45:55 +1100

    Flutter jumping!

---
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField]
+    private List<Collider2D> _ignoredColliders = new List<Collider2D>();
+    
+    private List<Collider2D> _overlappingColliders = new List<Collider2D>();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_ignoredColliders.Contains(other))
+            return;
+        
+        if (!_overlappingColliders.Contains(other))
+            _overlappingColliders.Add(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {       
+        if (_overlappingColliders.Contains(other))
+            _overlappingColliders.Remove(other);
+    }
+
+    public List<Collider2D> GetOverlappingColliders()
+    {
+        return _overlappingColliders;
+    }
+
+    public bool IsColliding()
+    {
+        return _overlappingColliders.Count > 0;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Runtime.InteropServices.ComTypes;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.Experimental.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float SinglePixel = 3.125f / 32f;
+
+    // Timer lengths
+    private const float JumpSafetyTimerTime = 0.3f;
+    private const float JumpButtonLeewayTimerTime = 0.15f;
+    private const float FlutterJumpTimerTime = 1.0f;
+    private const float FlutterJumpCooldownTimerTime = 0.151f;
+
+    [NotNull] [SerializeField] private GroundCheck _groundCheck;
+    [NotNull] private Rigidbody2D _rb;
+
+    private float _movementSpd = 30.0f * SinglePixel;
+    private float _jumpHeightImpulse = 9.0f;
+    private float _flutterJumpAmount = 2.4f;
+    private float _fastFallImpulse = 3.0f;
+    private float _gravitySpd = 5.0f * SinglePixel;
+
+    private int _inputDirX = 0;
+
+    private int _flutterJumpsBeforeLandingCount = 0;
+    private float _flutterJumpIncidentYVelocity = 0;
+
+    private bool _isJumpHeldDown = false;
+    private bool _isJumpJustPressed = false;
+    private bool _isJumpJustReleased = false;
+
+    // Timers
+    private float _jumpSafetyTimer = 0.0f;
+    private float _jumpButtonLeewayTimer = 0.0f;
+    private float _flutterJumpTimer = 0.0f;
+    private float _flutterJumpCooldownTimer = 0.0f;
+
+    // Stopwatches
+    private float _jumpHoldTime = -1.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void ProcessInput()
+    {
+        _inputDirX = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        _isJumpHeldDown = Input.GetButton("Jump");
+        _isJumpJustPressed = Input.GetButtonDown("Jump");
+        _isJumpJustReleased = Input.GetButtonUp("Jump");
+
+        if (Input.GetButtonDown("Jump"))
+            _jumpButtonLeewayTimer = JumpButtonLeewayTimerTime;
+
+        if (Input.GetButtonUp("Jump"))
+            _jumpButtonLeewayTimer = 0.0f;
+    }
+
+    private void HandleTimers()
+    {
+        _jumpSafetyTimer -= Time.fixedDeltaTime;
+        _jumpSafetyTimer = Mathf.Clamp(_jumpSafetyTimer, 0.0f, JumpSafetyTimerTime);
+
+        _jumpButtonLeewayTimer -= Time.fixedDeltaTime;
+        _jumpButtonLeewayTimer = Mathf.Clamp(_jumpButtonLeewayTimer, 0.0f, JumpButtonLeewayTimerTime);
+
+        _flutterJumpTimer -= Time.fixedDeltaTime;
+        _flutterJumpTimer = Mathf.Clamp(_flutterJumpTimer, 0.0f, FlutterJumpTimerTime);
+
+        _flutterJumpCooldownTimer -= Time.fixedDeltaTime;
+        _flutterJumpCooldownTimer = Mathf.Clamp(_flutterJumpCooldownTimer, 0.0f, FlutterJumpCooldownTimerTime);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        var movementSpdModifier = 1.0f;
+        var xSpd = _inputDirX * _movementSpd;
+
+        // Handle jumping
+        var canJump = CanJump();
+
+        // Jump if jump button is pressed within leeway amount
+        if (_jumpButtonLeewayTimer > 0.0f && canJump)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, 0.0f);
+            _rb.AddForce(_jumpHeightImpulse * Vector2.up, ForceMode2D.Impulse);
+            _jumpSafetyTimer = JumpSafetyTimerTime;
+            // Reset flutter jump count
+            _flutterJumpsBeforeLandingCount = 0;
+        }
+
+        if (_isJumpHeldDown)
+        {
+            if (_jumpHoldTime < 0.0f)
+                _jumpHoldTime = 0.0f;
+            _jumpHoldTime += Time.fixedDeltaTime;
+        }
+        else if (_isJumpJustReleased || CanJump())
+        {
+            _jumpHoldTime = -1.0f;
+        }
+
+        if (_isJumpJustReleased)
+        {
+            //_rb.velocity = new Vector2(_rb.velocity.x, 0.0f);
+            _rb.AddForce(_fastFallImpulse * Vector2.down, ForceMode2D.Impulse);
+        }
+
+        // Handle flutter jump
+
+        // If Yoshi is falling and jump is held down...
+        if ((_jumpButtonLeewayTimer > 0.0f || (_isJumpHeldDown && _flutterJumpsBeforeLandingCount < 1)) && _rb.velocity.y < -0.05f)
+        {
+            _flutterJumpIncidentYVelocity = _rb.velocity.y;
+            _flutterJumpsBeforeLandingCount++;
+            // If not already flutter jumping and cooldown is not active, make Yoshi flutter jump
+            if (_flutterJumpTimer <= 0.0f && _flutterJumpCooldownTimer <= 0.0f)
+            {
+                _flutterJumpTimer = FlutterJumpTimerTime;
+            }
+        }
+
+        if (!_isJumpHeldDown && _flutterJumpTimer > 0.2f)
+            _flutterJumpTimer = 0.1f;
+
+
+        // Do flutter jump stuff if flutter jumping
+        if (_flutterJumpTimer > 0.0f)
+        {
+            var YVelocityChange = _flutterJumpAmount *
+                                  (1.15f * Mathf.Sin(2 * (_flutterJumpTimer) * Mathf.PI + 0.25f) + 0.15f);
+            _rb.velocity = new Vector2(_rb.velocity.x, (0.7f * _flutterJumpIncidentYVelocity) + YVelocityChange + _gravitySpd);
+            _flutterJumpCooldownTimer = FlutterJumpCooldownTimerTime;
+
+            movementSpdModifier *= 0.55f;
+        }
+
+        // Handle movement 
+        var newVelocity = _rb.velocity;
+
+        newVelocity.x = xSpd * movementSpdModifier;
+        newVelocity.y -= _gravitySpd;
+
+        _rb.velocity = newVelocity;
+
+        HandleTimers();
+    }
+
+    private bool CanJump()
+    {
+        if (!_groundCheck.IsColliding())
+            return false;
+
+        if (_jumpSafetyTimer > 0.001)
+            return false;
+
+        return true;
+    }
+}
